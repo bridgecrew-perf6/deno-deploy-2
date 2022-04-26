43384fae17f56e0b31b267f185bc38d4587fd4b4
James Aviani <91283996+james-aviani@users.noreply.github.com>
2022-04-26 09:38:34 -0700

    Update router.ts

--- router.ts
+++ router.ts
@@ -1,11 +1,15 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 import { us_zip_codes } from "./us_zip_codes.ts";
 
+let postal = new us_zip_codes();
+
 function handler(_req: Request) {
+  let y = postal.get("93108");
   const data = {
     destination_phone_number: "+18443677787",
     source: "deno-deploy!",
     version: ".001",
+    location: JSON.stringify(y),
   };
   const body = JSON.stringify(data, null, 2);
   return new Response(body, {
