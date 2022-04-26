8325665897552d6bed621207af21b1ba17629af5
James Aviani <91283996+james-aviani@users.noreply.github.com>
2022-04-26 12:17:17 -0700

    Update router.ts

--- router.ts
+++ router.ts
@@ -4,16 +4,22 @@ import { us_zip_codes } from "./us_zip_codes.ts";
 let postal = new us_zip_codes();
 
 function handler(req: Request) {
-  let req_data = req.json();
+  let inputs = {"postal1":"nada","postal2":"nada"};
+  
+  if (inputs.method === "POST") {
+      inputs = await req.json();
+  }
    
-  let y = postal.get_miles("93108");
+  let miles = postal.get_miles(inputs.postal1,inputs.postal2);
+  
   const data = {
     destination_phone_number: "+18558380022,",
     source: "deno-deploy!",
     version: ".002",
-    location: JSON.stringify(y),
-    incoming: JSON.stringify(req_data),
+    location: miles,
+    incoming: JSON.stringify(inputs),
   };
+  
   const body = JSON.stringify(data, null, 2);
   return new Response(body, {
     headers: { "content-type": "application/json; charset=utf-8" },
