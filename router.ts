7fb10121295c15ab9b878dff2dfd617faa055099
James Aviani <91283996+james-aviani@users.noreply.github.com>
2022-05-09 22:42:49 -0700

    Update router.ts

--- router.ts
+++ router.ts
@@ -1,32 +1,14 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
-//import { us_zip_codes } from "./us_zip_codes.ts";
 
-//let postal = new us_zip_codes();
-
-function handler(req: Request) {
-  let inputs = {"postal1":"nada","postal2":"nada"};
-  
-  if (inputs.method === "POST") {
-      inputs = await req.json();
-  }
-   
-  //let miles = postal.get_miles(inputs.postal1,inputs.postal2);
-  let miles = 0.0;
-  
-  //  destination_phone_number: "+18558380022,",
-  const data = {   
-    source: "deno-deploy!",
-    version: ".002",
-    location: miles,
-    incoming: JSON.stringify(inputs),
+function handler(_req: Request) {
+  const data = {
+    Hello: "World!",
   };
-  
   const body = JSON.stringify(data, null, 2);
   return new Response(body, {
     headers: { "content-type": "application/json; charset=utf-8" },
   });
 }
 
-console.log("Listening...");
-
+console.log("Listening on http://localhost:8000");
 serve(handler);
